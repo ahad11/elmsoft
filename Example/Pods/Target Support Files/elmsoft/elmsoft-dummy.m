a697ac065ffcc8c68c79f95afb4200fd7477434a
ahad375 <aalarifi64@gmail.com>
2017-11-05 11:21:24 +0300

    initial

---
+++ Example/Pods/Target Support Files/elmsoft/elmsoft-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_elmsoft : NSObject
+@end
+@implementation PodsDummy_elmsoft
+@end
---
+++ elmsoft/Classes/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  elmsoft
+//
+//  Created by ahad on 11/5/17.
+//
+
+import Foundation
+public class elm{
+    public func elmsoft(){
+    print("elmsoft")
+    
+    }
+    
+}
